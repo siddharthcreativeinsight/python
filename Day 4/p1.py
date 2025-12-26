26205cf9d49591180823fda588454ec92ea72fdd
siddharthcreativeinsight <siddharth.creativeinsight@gmail.com>
2025-12-26 17:09:01 +0530

    Add files via upload

---
+++ Day 4/p1.py
@@ -0,0 +1,9 @@
+#the programmer for number divislble
+user=int(input("enter the number:"))
+def divison1(user):
+     if user%5==0:
+         print("yes,Answer is 0")
+     else:
+         print("no, Answer is not 0")
+divison1(user)
+
